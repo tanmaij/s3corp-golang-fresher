7923d176b324a4b3b4b8ca6dc09faee0cb15d883
tanmaij <tanmaijnguyen@gmail.com>
2022-06-24 11:14:33 +0700

    Revert "Revert "working on user features""
    This reverts commit 60d5862a16ea7157327ee4f71d86f3c55649ea18.

--- internal/service/userServiceImpl.go
+++ internal/service/userServiceImpl.go
@@ -18,29 +18,29 @@ func NewUserService(userRepo repository.UserRepo) UserService {
 func (userServiceImpl UserServiceImpl) Login(username string, password string) (*models.User, utils.Error) {
 	user, err := userServiceImpl.UserRepo.Login(username)
 	if err != nil {
-		return user, utils.NewError("Internal Server Error", http.StatusInternalServerError)
+		return user, utils.NewError(utils.InternalServerError, http.StatusInternalServerError)
 	}
 	if user.Password.String != password {
-		return user, utils.NewError("Password is incorrect", http.StatusUnauthorized)
+		return user, utils.NewError(utils.PasswordIsIncorrect, http.StatusUnauthorized)
 	}
-	return user, utils.NewError("Successfully", http.StatusOK)
+	return user, utils.NewError(utils.Successfully, http.StatusOK)
 }
 
 func (userServiceImpl UserServiceImpl) GetUserByUsername(username string) (*models.User, utils.Error) {
 	user, err := userServiceImpl.UserRepo.GetUserByUsername(username)
 
 	if err != nil {
-		return user, utils.NewError("Internal Server Error", http.StatusInternalServerError)
+		return user, utils.NewError(utils.InternalServerError, http.StatusInternalServerError)
 	}
-	return user, utils.NewError("Successfully", http.StatusOK)
+	return user, utils.NewError(utils.Successfully, http.StatusOK)
 }
 
 func (userServiceImpl UserServiceImpl) GetUsers() (models.UserSlice, utils.Error) {
 	users, err := userServiceImpl.UserRepo.GetUsers()
 	if err != nil {
-		return users, utils.NewError("Internal Server Error", http.StatusInternalServerError)
+		return users, utils.NewError(utils.InternalServerError, http.StatusInternalServerError)
 	}
-	return users, utils.NewError("Successfully", http.StatusOK)
+	return users, utils.NewError(utils.Successfully, http.StatusOK)
 }
 
 func (userServiceImpl UserServiceImpl) CreateUser(user models.User) utils.Error {
@@ -48,34 +48,35 @@ func (userServiceImpl UserServiceImpl) CreateUser(user models.User) utils.Error 
 	_, err := userServiceImpl.UserRepo.GetUserByUsername(user.Username)
 
 	if err == nil {
-		return utils.NewError("User already exist", http.StatusBadRequest)
+		return utils.NewError(utils.UserAlreadyExist, http.StatusBadRequest)
 	}
 
 	err = userServiceImpl.UserRepo.CreateUser(user)
 	if err != nil {
-		return utils.NewError("Internal Server Error", http.StatusInternalServerError)
+		return utils.NewError(utils.InternalServerError, http.StatusInternalServerError)
 	}
-	return utils.NewError("Successfully", http.StatusOK)
+	return utils.NewError(utils.Successfully, http.StatusOK)
 }
 
 func (userServiceImpl UserServiceImpl) UpdateUser(user models.User) utils.Error {
 	affectedRows, err := userServiceImpl.UserRepo.UpdateUser(user)
-	if err != nil {
-		return utils.NewError("Internal Server Error", http.StatusInternalServerError)
-	}
+
 	if affectedRows <= 0 {
-		return utils.NewError("The user is not exist", http.StatusNotFound)
+		return utils.NewError(utils.NotExist, http.StatusNotFound)
 	}
-	return utils.NewError("Successfully", http.StatusOK)
+	if err != nil {
+		return utils.NewError(utils.Successfully, http.StatusInternalServerError)
+	}
+	return utils.NewError(utils.Successfully, http.StatusOK)
 }
 
 func (userServiceImpl UserServiceImpl) DeleteUser(username string) utils.Error {
 	affectedRows, err := userServiceImpl.UserRepo.DeleteUser(username)
-	if err != nil {
-		return utils.NewError("Internal Server Error", http.StatusInternalServerError)
-	}
 	if affectedRows <= 0 {
-		return utils.NewError("The user is not exist", http.StatusNotFound)
+		return utils.NewError(utils.NotExist, http.StatusNotFound)
 	}
-	return utils.NewError("Successfully", http.StatusOK)
+	if err != nil {
+		return utils.NewError(utils.InternalServerError, http.StatusInternalServerError)
+	}
+	return utils.NewError(utils.Successfully, http.StatusOK)
 }
